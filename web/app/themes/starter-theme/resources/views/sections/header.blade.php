7e1ac9e18507e42bcb4c72bb4482f272843cb616
Matthew Hobbs-Hurrell <matthew@wecreatedigital.co.uk>
2023-06-03 20:48:23 +0100

    Add basic content to site header

--- web/app/themes/starter-theme/resources/views/sections/header.blade.php
+++ web/app/themes/starter-theme/resources/views/sections/header.blade.php
@@ -1,10 +1,10 @@
-<header class="banner">
+<header class="banner px-10 py-2 flex justify-between items-center">
   <a class="brand" href="{{ home_url('/') }}">
-    {!! $siteName !!}
+    <img class="h-14" src="{!! get_theme_file_uri('/resources/images/one-stop-inventories-cropped.png') !!}" alt="One Stop Inventories Logo" />
   </a>
 
   @if (has_nav_menu('primary_navigation'))
-    <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
+    <nav class="nav-primary flex items-center" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
       {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
     </nav>
   @endif
